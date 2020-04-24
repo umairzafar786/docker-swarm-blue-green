d9bc49fa6b3753ba199f9005ac4b703897b9ae0e
umairzafar786 <53565411+umairzafar786@users.noreply.github.com>
2020-04-24 13:03:34 +0500

    Update deploy-images.sh

--- deploy-images.sh
+++ deploy-images.sh
@@ -2,5 +2,5 @@
 
 docker build load-balancer -t umairzafar786/swarm-lb:0.0.1
 docker push umairzafar786/swarm-lb:0.0.1
-docker build swarm-backend -t cecchisandrone/swarm-backend:0.0.1
+docker build swarm-backend -t umairzafar786/swarm-backend:0.0.1
 docker push umairzafar786/swarm-backend:0.0.1
